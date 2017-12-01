4cf2a44677922c21142622f2722f86e66ea15d42
Jakub Gejdos <gejdosjakub@gmail.com>
2017-12-01 20:54:04 +0100

    Pole

---
+++ DniTyzdnaHodina/Pole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DniTyzdnaHodina
+{
+    class Pole
+    {
+        //pole - vzdy referencny typ
+        public int[] VnutornePole = new int[4] { 10, 4, 5, 6 };
+
+
+
+
+    }
+}
